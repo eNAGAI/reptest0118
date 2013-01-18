f34c9315002d78e62d3b0642d6bb185a563bc499
eNAGAI <ngi@nts.ricoh.co.jp>
2013-01-18 18:10:08 +0900

    test commit

---
+++ testHelloWorld/src/com/example/androidhelloworld/test/MainActivityTest.java
@@ -0,0 +1,33 @@
+package com.example.androidhelloworld.test;
+
+import com.example.androidhelloworld.MainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+		private MainActivity activity;
+		private TextView view;
+		private Object resourceString;
+		@SuppressWarnings("deprecation")
+		public MainActivityTest() {
+		super("com.example.androidhelloworld", MainActivity.class);
+		// TODO Auto-generated constructor stub
+	}
+@Override
+		protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+		activity = getActivity();
+
+		view = (TextView) activity.findViewById(com.example.androidhelloworld.R.id.textView);
+
+		resourceString = activity.getString(com.example.androidhelloworld.R.string.hello_world);
+		}
+
+		public void testname() throws Exception {
+			assertNotNull(view.getText().toString(), resourceString);
+		}
+
+}
